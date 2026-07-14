94eba499ee17e7a19a66c335155b83c063a9b813
Raaj Patil <tujha-github-email@gmail.com>
2026-07-14 13:37:22 +0530

    uploading backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Database Connected'))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
